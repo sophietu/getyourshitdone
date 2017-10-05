bdd18df134cb50dd96e40f2059f14687c7eb1210
Christian Galang <cg.mico@gmail.com>
2017-10-05 02:42:09 +0000

    Added C9 work changes to send email + front-end

--- client/main.js
+++ client/main.js
@@ -3,20 +3,24 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.emailSubmission.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
 
-Template.hello.helpers({
+Template.emailSubmission.helpers({
   counter() {
     return Template.instance().counter.get();
   },
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
+Template.emailSubmission.events({
+    'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
+    Meteor.call(
+      'sendEmail', // function to call
+      'hamaad.markhiani@gmail.com', // email address the message will be sent to (change this to text box input later)
+      'emailString'); // text of the actual email (change this to text box input later)
   },
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,17 @@
 import { Meteor } from 'meteor/meteor';
+import { Email } from 'meteor/email';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  process.env.MAIL_URL = 'smtps://postmaster@sandbox35a8958a71974f2c82402afd5a59e150.mailgun.org:e11ba6fced7028d454429287b0adfc1f@smtp.mailgun.org:587';
+});
+
+Meteor.methods({
+    sendEmail(to, text) {
+        
+        this.unblock();
+        
+        Email.send({ to: to, from: 'getYourShitDone@google.com', subject: 'You have a new reminder!', text: text });
+        
+    }
 });
